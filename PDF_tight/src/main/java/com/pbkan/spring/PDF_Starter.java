7dee0cac399309cc441a694fb9896c9902f0d9d8
pbkan <pbkan4@gmail.com>
2025-06-29 21:32:52 +0900

    [박병준]개인PJ 초기설정

---
+++ PDF_tight/src/main/java/com/pbkan/spring/PDF_Starter.java
@@ -0,0 +1,13 @@
+package com.pbkan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDF_Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PDF_Starter.class, args);
+	}
+
+}
